f22c9e33cab1164392026d4e25e2a2ae184530af
Abdullah Mudathir <abdullah.mudathir@isslng.com>
2023-05-29 21:48:38 +0100

    Styling profile

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,17 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<house.Explore />} />
-          <Route path="/offers" element={<house.Offers />} />
-          <Route path="/profile" element={<house.Profile />} />
-          <Route path="/sign-in" element={<house.SignIn />} />
-          <Route path="/sign-up" element={<house.SignUp />} />
-          <Route path="/forgot-password" element={<house.ForgotPassword />} />
-        </Routes>
-        <Navbar />
+        <div className="pageContainer">
+          <Routes>
+            <Route path="/" element={<house.Explore />} />
+            <Route path="/offers" element={<house.Offers />} />
+            <Route path="/profile" element={<house.Profile />} />
+            <Route path="/sign-in" element={<house.SignIn />} />
+            <Route path="/sign-up" element={<house.SignUp />} />
+            <Route path="/forgot-password" element={<house.ForgotPassword />} />
+          </Routes>
+          <Navbar />
+        </div>
       </Router>
       <ToastContainer />
     </div>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getAuth } from "firebase/auth";
+import { setDoc, updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { getAuth, updateProfile } from "firebase/auth";
+import { toast } from "react-toastify";
 export function Profile() {
   const [user, setUser] = useState<any>(null);
+  const [changeDetails, setChangeDetails] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const auth = getAuth();
@@ -11,27 +15,84 @@ export function Profile() {
     email: auth.currentUser?.email,
   });
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setFormData((prev: any) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
 
-  const {name,email} = formData;
-  const logOut = () =>{
+  // Destructured the form data
+  const { name, email } = formData;
+
+  const logOut = () => {
     auth.signOut();
-    navigate('/sign-in');
-  }
+    navigate("/sign-in");
+  };
+
+  const onSubmit = async () => {
+    console.log("hello");
+    try {
+      if (auth.currentUser?.displayName !== name) {
+        // Update display name in firebase
+        await updateProfile(auth.currentUser!, {
+          displayName: name,
+        });
+
+        // Update in firestore
+        const userRef = doc(db, "users", auth.currentUser!.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (ex) {
+      toast.error('Could not update the user profile')
+    }
+  };
   useEffect(() => {
-    console.log(auth.currentUser);
     setUser(auth.currentUser);
   }, []);
 
   return (
     <div className="profile">
       <header className="profile__header">
-        <p className="pageHeader">
-          My Profile
-        </p>
+        <p className="pageHeader">My Profile</p>
         <button type="button" onClick={logOut} className="logOut">
           Logout
         </button>
       </header>
+      <main>
+        <div className="profile__details details">
+          <p className="details__text">Personal Details</p>
+          <p
+            className="details__changePersonalDetails"
+            onClick={() => setChangeDetails((prev) => !prev)}
+          >
+            {changeDetails ? "Done" : "Change"}
+          </p>
+        </div>
+
+        <div className="profile__card card">
+          <form action="" autoComplete="off">
+            <label htmlFor="name">Profile Name:</label>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "card__name" : "card__name-active"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "card__email" : "card__email-active"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+            <input type="submit" value="Make Changes" />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
